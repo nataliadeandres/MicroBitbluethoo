d92e8bda1e8bdf603c993d74d10d842933ffcf63
nataliadeandres <123065922+nataliadeandres@users.noreply.github.com>
2023-01-24 10:06:25 +0100

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,23 @@
+bluetooth.startUartService()
 
+pins.P0.analogWrite(0)
+pins.P0.analogSetPeriod(100)
+
+bluetooth.onUartDataReceived(bluetooth.NEW_LINE, () => {
+    let command = bluetooth.uartReadUntil(bluetooth.NEW_LINE).split(" ")
+    let dispositivo = command[0]
+    let value = parseInt(command[1])
+
+    basic.showNumber(value)
+
+    if (dispositivo == "LUZ") {
+        pins.P0.analogWrite(pins.map(value, 0, 100, 0, 1023))
+
+    }
+    else if (dispositivo == "LOCK") {
+        pins.P1.digitalWrite((value == 1) ? true : false)
+    }
+    else {
+        basic.showString("E")
+    }
+})
